be4eb8a3ff36b55409c1b5c89daa94f43b6727f8
marianboss21 <115264653+marianboss21@users.noreply.github.com>
2023-05-25 13:40:07 +0200

    Add files via upload

---
+++ GUI/GlobalState.java
@@ -0,0 +1,33 @@
+package GUI;
+
+/*
+ * Contains the variables that are used all across the files
+ */
+import java.awt.*;
+
+public class GlobalState {
+    private static final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    public static final int FRAME_WIDTH = (int) screenSize.getWidth();
+    public static final int FRAME_HEIGHT = (int) screenSize.getHeight();
+
+    public static boolean paused = false;
+
+    public static int simulationSpeedIncrement = 1;
+    public static int simulationSpeed = 10;
+
+    public static int SCALE = 2000000;
+
+    public static PlanetStats planetFocused = new PlanetStats(null, 0, null);
+
+    public static int getFocusShiftX() {
+        return planetFocused.positionX / SCALE;
+    }
+
+    public static int getFocusShiftY() {
+        return planetFocused.positionY / SCALE;
+    }
+
+    public static int[] getCenter() {
+        return new int[] { FRAME_WIDTH / 2, FRAME_HEIGHT / 2 };
+    }
+}
---
+++ GUI/Planet.java
@@ -0,0 +1,71 @@
+package GUI;
+
+/*
+ * Contains methods for creating and coloring the Planet
+ */
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+
+public class Planet extends Drawable {
+
+	@Override
+	public void paintComponent(Graphics g) {
+		this.g2 = (Graphics2D) g;
+
+		this.draw(g2);
+	}
+
+	private PlanetStats planetStats;
+	private Graphics2D g2;
+
+	private int planetPositionX = 0;
+	private int planetPositionY = 0;
+
+	// Constructor for creating the planetStats
+	public Planet(PlanetStats planetStats) {
+		this.planetStats = planetStats;
+	}
+
+	// Method for creating the specific planet on specific position
+	public void draw(Graphics2D g2) {
+		g2.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING,
+				java.awt.RenderingHints.VALUE_ANTIALIAS_ON);
+
+		calculatePosition();
+		drawPlanetName();
+
+		g2.setStroke(new java.awt.BasicStroke(3));
+		g2.setColor(planetStats.color);
+		g2.draw(getPlanet());
+	}
+
+	// Calculates the position of the celestialBody
+	private void calculatePosition() {
+		this.planetPositionX = (int) (planetStats.positionX / GlobalState.SCALE
+				+ GlobalState.getCenter()[0]
+				- GlobalState.getFocusShiftX()
+				- planetStats.size / 2);
+		this.planetPositionY = (int) (planetStats.positionY / GlobalState.SCALE
+				+ GlobalState.getCenter()[1]
+				- GlobalState.getFocusShiftY()
+				- planetStats.size / 2);
+	}
+
+	private void drawPlanetName() {
+		g2.setColor(Style.fontColor);
+		g2.setFont(Style.font);
+		g2.drawString(planetStats.name,
+				planetPositionX
+						- planetStats.name.length() * 3,
+				planetPositionY);
+	}
+
+	private Ellipse2D.Double getPlanet() {
+		return new Ellipse2D.Double(
+				planetPositionX,
+				planetPositionY,
+				planetStats.size - (int) (GlobalState.SCALE / 500000),
+				planetStats.size - (int) (GlobalState.SCALE / 500000));
+	}
+}
